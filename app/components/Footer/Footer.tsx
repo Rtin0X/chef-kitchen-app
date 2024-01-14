b26fca37b10e994711ddf69b57f7bccde84ab409
Samy <145269734+Rtin0X@users.noreply.github.com>
2024-01-14 23:19:38 +0100

    first commit

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,116 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+interface ProductType {
+    id: number;
+    section: string;
+    link: string[];
+}
+
+interface socialLinks {
+    imgSrc: string;
+    link: string;
+    width: number;
+}
+
+const socialLinks: socialLinks[] = [
+    {
+        imgSrc: '/images/Footer/facebook.svg',
+        link: 'https://facebook.com',
+        width: 10
+    },
+    {
+        imgSrc: '/images/Footer/insta.svg',
+        link: 'https://instagram.com',
+        width: 14
+    },
+    {
+        imgSrc: '/images/Footer/twitter.svg',
+        link: 'https://twitter.com',
+        width: 14
+    },
+
+]
+
+const products: ProductType[] = [
+    {
+        id: 1,
+        section: "Company",
+        link: ['A propos', 'Carrieres', 'Mobile', 'Blog', 'Savoir-faire'],
+    },
+    {
+        id: 2,
+        section: "Contact",
+        link: ['FAQ', 'Infos', 'Affiliations', 'Partenaires']
+    }
+    ,
+    {
+        id: 3,
+        section: "Plus",
+        link: ['Recettes', 'Chefs', 'Ingredients', 'Support']
+    }
+]
+
+const footer = () => {
+    return (
+
+        <div className="mx-auto max-w-2xl pt-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="my-12 grid grid-cols-1 gap-y-10 sm:grid-cols-6 lg:grid-cols-12">
+
+                {/* COLUMN-1 */}
+
+                <div className='sm:col-span-6 lg:col-span-5'>
+                    <div className="flex flex-shrink-0 items-center border-right">
+                        <Image src="/images/Logo/Logo.svg" alt="logo" width={56} height={56} />
+                        <Link href="/" className='text-2xl font-semibold text-black ml-4'>
+                            Chef&apos;s Kitchen.
+                        </Link>
+                    </div>
+                    <h3 className='text-textbl text-xs font-medium mt-5 mb-4 lg:mb-16'> </h3>
+                    <div className='flex gap-4'>
+
+                        {socialLinks.map((items, i) => (
+                        <Link href={items.link} key={i}>
+                            <div className="bg-white h-10 w-10 shadow-xl text-base rounded-full flex items-center justify-center footer-icons hover:bg-pink">
+                                <Image src={items.imgSrc} alt={items.imgSrc} width={items.width} height={2} className="sepiaa" />
+                            </div>
+                        </Link>
+                        ))}
+
+                    </div>
+                </div>
+
+                {/* CLOUMN-2/3/4 */}
+
+
+                {products.map((product) => (
+                    <div key={product.id} className="sm:col-span-2">
+                        <p className="text-black text-xl font-semibold mb-9">{product.section}</p>
+                        <ul>
+                            {product.link.map((link: string, index: number) => (
+                                <li key={index} className='mb-5'>
+                                    <Link href="/" className="text-footerlinks text-base font-normal mb-6 space-links">{link}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+
+            </div>
+
+            {/* All Rights Reserved */}
+
+            <div className='py-10 md:flex items-center justify-between border-t border-t-bordertop'>
+                <h4 className='text-darkgrey text-sm text-center md:text-start font-normal'>@2024 - Chef&apos;s kitchen. Tous les droits sont réservés par Samy</h4>
+                <div className="flex gap-5 mt-5 md:mt-0 justify-center md:justify-start">
+                    <h4 className='text-darkgrey text-sm font-normal'><Link href="/" target="_blank">Vie Privée</Link></h4>
+                    <div className="h-5 bg-bordertop w-0.5"></div>
+                    <h4 className='text-darkgrey text-sm font-normal'><Link href="/" target="_blank">Termes & Conditions</Link></h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default footer;
